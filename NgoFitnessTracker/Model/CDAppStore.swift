442a59fc9d5eb9b7136bf7bc708185355b9889a0
Kevin Ngo <ngokevin39@gmail.com>
2020-03-02 22:58:14 -0500

    cleanup

--- NgoFitnessTracker/Model/CDAppStore.swift
+++ NgoFitnessTracker/Model/CDAppStore.swift
@@ -1,16 +1,9 @@
 //
-//  CDState+CoreDataClass.swift
+//  CDAppStore.swift
 //  NgoFitnessTracker
 //
 //  Created by Kevin Ngo on 2020-03-02.
 //  Copyright © 2020 Kevin Ngo. All rights reserved.
 //
-//
 
 import Foundation
-import CoreData
-
-@objc(CDState)
-public class CDState: NSManagedObject {
-
-}
